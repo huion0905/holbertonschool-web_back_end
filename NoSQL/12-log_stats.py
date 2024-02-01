5fe9ec3bd6c2fc9dfa743e4a307566653555e610
huion0905 <valery.armas@upch.pe>
2024-02-01 09:50:16 -0800

    Python script that provides some stats about Nginx logs stored in MongoDB

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -16,8 +16,10 @@ def log_stats():
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    status_check_count = nginx_collection.count_documents(
+            {"method": "GET", "path": "/status"})
     print(f"{status_check_count} status check")
+
 
 if __name__ == "__main__":
     log_stats()
