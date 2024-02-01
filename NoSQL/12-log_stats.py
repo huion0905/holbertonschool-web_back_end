d54c6922ca90ea9e0109aa545ff8943add5a4964
huion0905 <valery.armas@upch.pe>
2024-02-01 10:03:56 -0800

    Write a Python script that provides some stats about Nginx logs stored in MongoDB

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,19 +3,38 @@
 from pymongo import MongoClient
 
 
-def log_stats():
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+def log_stats(mongo_collection):
+    """Provides stats about Nginx logs stored in logs database
+    and nginx collection"""
+    # Get the total number of logs
+    total_logs = mongo_collection.count_documents({})
 
-    logs_count = nginx_collection.count_documents({})
-    print(f"{logs_count} logs")
-
+    # Get the number of logs for each HTTP method
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    method_counts = {method: mongo_collection.count_documents(
+            {"method": method}) for method in methods}
 
-    status_check_count = nginx_collection.count_documents(
+    # Get the number of logs for a specific method and path
+    specific_log_count = mongo_collection.count_documents(
             {"method": "GET", "path": "/status"})
+
+    # Get the number of status check logs
+    status_check_count = mongo_collection.count_documents({"path": "/status"})
+
+    return total_logs, method_counts, specific_log_count, status_check_count
+
+
+if __name__ == "__main__":
+    # Connect to MongoDB
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    # Get and print the log statistics
+    total_logs, method_counts, specific_log_count, \
+        status_check_count = log_stats(logs_collection)
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"\tmethod {method}: {count}")
     print(f"{status_check_count} status check")
