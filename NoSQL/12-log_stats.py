e7b3ac431f48b16c132f70f844495aaa5a145fb4
huion0905 <valery.armas@upch.pe>
2024-02-01 09:47:12 -0800

    Python script that provides some stats about Nginx logs stored in MongoDB

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,38 +3,21 @@
 from pymongo import MongoClient
 
 
-def log_stats(mongo_collection):
-    """Provides stats about Nginx logs stored in logs database
-    and nginx collection"""
-    # Get the total number of logs
-    total_logs = mongo_collection.count_documents({})
+def log_stats():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-    # Get the number of logs for each HTTP method
+    logs_count = nginx_collection.count_documents({})
+    print(f"{logs_count} logs")
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: mongo_collection.count_documents(
-            {"method": method}) for method in methods}
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    # Get the number of logs for a specific method and path
-    specific_log_count = mongo_collection.count_documents(
-            {"method": "GET", "path": "/status"})
-
-    # Get the number of status check logs
-    status_check_count = mongo_collection.count_documents({"path": "/status"})
-
-    return total_logs, method_counts, specific_log_count, status_check_count
-
+    status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
 
 if __name__ == "__main__":
-    # Connect to MongoDB
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-
-    # Get and print the log statistics
-    total_logs, method_counts, specific_log_count,\
-    status_check_count = log_stats(logs_collection)
-
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check_count} status check")
+    log_stats()
