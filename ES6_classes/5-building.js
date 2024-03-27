dbf93245341ba6d9376663b7119e150ba405f1ed
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-27 12:17:02 -0500

    A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,31 @@
+export default class Building {
+    constructor(sqft) {
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      this._sqft = sqft;
+  
+      if (this.constructor !== Building) {
+        this.evacuationWarningMessage();
+      }
+    }
+  
+    // Getter and setter for sqft
+    get sqft() {
+      return this._sqft;
+    }
+  
+    set sqft(newValue) {
+      if (typeof newValue !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      this._sqft = newValue;
+    }
+  
+    // eslint-disable-next-line class-methods-use-this
+    evacuationWarningMessage() {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+  }
